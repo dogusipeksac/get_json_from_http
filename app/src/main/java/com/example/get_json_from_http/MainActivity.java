b75926148415ea261850f846e75d924f26345359
dogus ipeksac <dogus.ads.1998@hotmail.com>
2021-07-14 13:38:27 +0300

    get json from http

---
+++ app/src/main/java/com/example/get_json_from_http/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.get_json_from_http;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.searchText);
+    }
+
+    public void searchButton(View view) {
+
+
+        String url="http://api.plos.org/search?q=title:%22"+editText.getText().toString()+"%22%20AND%20body:%22RNA%22&fl=id,abstract&wt=json";
+        new MyAsyncTaskgetNews().execute(url);
+    }
+    // get news from server
+    public class MyAsyncTaskgetNews extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            //before works
+        }
+        @Override
+        protected String  doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            try {
+                String NewsData;
+                //define the url we have to connect with
+                URL url = new URL(params[0]);
+                //make connect with url and send request
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                //waiting for 7000ms for response
+                urlConnection.setConnectTimeout(7000);//set timeout to 5 seconds
+
+                try {
+                    //getting the response data
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    //convert the stream to string
+                    NewsData = ConvertInputToStringNoChange(in);
+                    //send to display data
+                    publishProgress(NewsData);
+                } finally {
+                    //end connection
+                    urlConnection.disconnect();
+                }
+
+            }catch (Exception ex){}
+            return null;
+        }
+        protected void onProgressUpdate(String... progress) {
+
+            try {
+                //display response data
+                JSONObject json=new JSONObject();
+                JSONObject query=json.getJSONObject("query");
+                JSONObject result=query.getJSONObject("abstract");
+                //burada istediğimiz queryi yazarız
+                Toast.makeText(getApplicationContext(),progress[0],Toast.LENGTH_LONG).show();
+
+            } catch (Exception ex) {
+            }
+
+
+        }
+
+        protected void onPostExecute(String  result2){
+
+
+        }
+
+
+
+
+    }
+
+    // this method convert any stream to string
+    public static String ConvertInputToStringNoChange(InputStream inputStream) {
+
+        BufferedReader bureader=new BufferedReader( new InputStreamReader(inputStream));
+        String line ;
+        String linereultcal="";
+
+        try{
+            while((line=bureader.readLine())!=null) {
+
+                linereultcal+=line;
+
+            }
+            inputStream.close();
+
+
+        }catch (Exception ex){}
+
+        return linereultcal;
+    }
+
+}
